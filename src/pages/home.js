878ae2d1a5a6210dec140fec595be13cf204160c
natassh <natacha.im@gmail.com>
2019-09-22 14:32:05 +0200

    treasas

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,21 @@
 import SlimSelect from 'slim-select';
-import '../../node_modules/slim-select/dist/SlimSelect.min.css';
+// import '../../node_modules/slim-select/dist/SlimSelect.min.css';
 import { getPhotosDogs } from '../modules/dogs/photos/index.js';
 import { loadBreedsOptions } from '../modules/dogs/breeds/index.js';
 import { stackInit } from '../modules/used-stack/index.js';
 import '../../css/styles.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-    stackInit();
+  stackInit();
 
-    new SlimSelect({
-        select: '#dog-selector'
-      });
+  new SlimSelect({
+    select: '#dog-selector'
+  });
 
-    loadBreedsOptions();
+  loadBreedsOptions();
 });
 
-document.querySelector('#btn-submit').addEventListener('click', (e) => {
-    e.preventDefault();
-    getPhotosDogs();
+document.querySelector('#btn-submit').addEventListener('click', e => {
+  e.preventDefault();
+  getPhotosDogs();
 });
