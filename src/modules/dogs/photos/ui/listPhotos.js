9464e023ed4c94360bd5afaf2ac3ff79d61e291a
natassh <natacha.im@gmail.com>
2019-09-24 14:11:37 +0200

    🚀 - Created carousel section dynamically

--- src/modules/dogs/photos/ui/listPhotos.js
+++ src/modules/dogs/photos/ui/listPhotos.js
@@ -2,7 +2,18 @@ import Carousel from 'marvina-carousel';
 import './listPhotos.css';
 
 const showPhotos = photos => {
-  document.querySelector('.list-photos').innerHTML = '';
+  createAndAddSectionContainer();
+  showPhotosInPage(photos);
+};
+
+const createAndAddSectionContainer = () => {
+  const sectionContainer = document.createElement('section');
+  sectionContainer.id = 'carousel';
+  sectionContainer.className = 'list-photos';
+  document.querySelector('.main-content').appendChild(sectionContainer);
+};
+
+const showPhotosInPage = photos => {
   photos.message.forEach(photo => {
     let html = '';
     html += `<img src="${photo}"/>`;
@@ -12,6 +23,10 @@ const showPhotos = photos => {
     document.querySelector('.list-photos').appendChild(figure);
   });
 
+  initPluginCarousel();
+};
+
+const initPluginCarousel = () => {
   const carousel = new Carousel({
     el: '#carousel',
     minImage: 2
