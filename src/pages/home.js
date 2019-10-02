f60a62eab024e1c066fd2e8f63b78256ee1e11f3
natassh <natacha.im@gmail.com>
2019-10-02 13:13:35 +0200

    Add slim css select

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,8 +3,6 @@ import { usedStackInit } from '../modules/used-stack/index.js';
 import { initLayout } from './layout/index.js';
 import './home.css';
 
-console.log('aaa2');
-
 document.addEventListener('DOMContentLoaded', () => {
   initLayout();
   usedStackInit();
